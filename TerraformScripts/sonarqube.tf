234a28641a27493f280b74c4265587085c42577c
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2023-02-18 15:46:10 +0530

    Update sonarqube.tf

--- TerraformScripts/sonarqube.tf
+++ TerraformScripts/sonarqube.tf
@@ -9,18 +9,9 @@ resource "aws_instance" "sonarqube" {
   instance_type = "t2.medium"             # Use t2.medium instance type
   key_name      = "Myserver1"         # Replace with your key name
   #vpc_security_group_ids = ["sg-0123456789abcdef"]  # Replace with your security group ID
-
-  # User data script to install SonarQube
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install java-1.8.0-openjdk -y
-              sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.2.46101.zip
-              sudo unzip sonarqube-8.9.2.46101.zip
-              sudo mv sonarqube-8.9.2.46101 /opt/sonarqube
-              sudo /opt/sonarqube/bin/linux-x86-64/sonar.sh start
-              EOF
-}
+  tag = {
+    Name = "SonarQubeServer"
+  }
 
 # Define an output for the instance's public IP address
 output "sonarqube_public_ip" {
