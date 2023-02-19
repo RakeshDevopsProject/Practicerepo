92344d3adc0ea4e8a4a40fbe67b64215142b95e1
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2023-02-19 12:03:14 +0530

    Update sonarqube.tf

--- TerraformScripts/sonarqube.tf
+++ TerraformScripts/sonarqube.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "sonarqube" {
   ami           = "ami-0c9978668f8d55984"  # SonarQube AMI for us-west-2
   instance_type = "t2.medium"             # Use t2.medium instance type
   key_name      = "Myserver1"         # Replace with your key name
-  security_group = ["launch-wizard-25"]
+  #security_group = ["launch-wizard-25"]
   #vpc_security_group_ids = ["sg-0123456789abcdef"]  # Replace with your security group ID
   tags = {
     Name = "SonarQubeServer"
