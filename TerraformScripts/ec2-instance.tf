88591da66574ca5f29e35c7fbf174d179e7f035f
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2023-02-20 10:03:18 +0530

    Update ec2-instance.tf

--- TerraformScripts/ec2-instance.tf
+++ TerraformScripts/ec2-instance.tf
@@ -1,12 +1,12 @@
-provider "aws" { 
+provider "aws" "tomcatserver"{ 
 	region = "us-east-1"
 } 
 resource "aws_instance" "AwsInstance"{
 	ami = "ami-0c9978668f8d55984"
 	instance_type = "t2.micro"
 	key_name = "Myserver1"
-	security_groups = ["launch-wizard-44"]
+	#security_groups = ["launch-wizard-44"]
 	tags = {
-	  Name = "Rakeshserver"
+	  Name = "tomcatserver"
     }
 }
